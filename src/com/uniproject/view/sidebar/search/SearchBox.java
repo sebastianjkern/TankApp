d449392d2a613e444cbf75caac5cbc7989b8a055
Sebastian Kern <sebastianjakobkern@gmail.com>
2023-07-11 16:14:49 +0200

    Cleanup unused imports

--- src/com/uniproject/view/sidebar/search/SearchBox.java
+++ src/com/uniproject/view/sidebar/search/SearchBox.java
@@ -1,12 +1,9 @@
 package com.uniproject.view.sidebar.search;
 
 import com.uniproject.controller.MainController;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.text.Text;
